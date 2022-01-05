ee3e0bb973f691e5363810ff64cbd757e476952d
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2022-01-05 11:44:56 -0300

    Operação de listagem, pesquisa e adição implementadas

--- controllers/stock.js
+++ controllers/stock.js
@@ -1,6 +1,16 @@
 const Stock = require("../models/stock-model")
 module.exports = app => {
-    app.get("/", (req, res) => {
+    app.get("/stock", (req, res) => {
         Stock.list(res)
+    })
+
+    app.get("/stock/:value", (req, res) => {
+        const value = (req.params.value)
+        Stock.search(res, value)
+    })
+
+    app.post("/stock/add", (req, res) => {
+        const values = req.body
+        Stock.add(res, values)
     })
 }
--- models/stock-model.js
+++ models/stock-model.js
@@ -10,6 +10,50 @@ class Stock{
             }
         })
     }
+
+    alter(res, values, id){
+        const sql = "update stock set ? where id = ?"
+        connection.query(sql, [values, id],(erro ,result) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(result)
+            }
+        })
+    }
+
+    delete(res, id){
+        const sql = "delete from product where id = ?"
+        connection.query(sql, id, (erro, result) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json({id})
+            }
+        })
+    }
+
+    add(res, values){
+        const sql = "insert into product set ?"
+        connection.query(sql, values, (erro, result) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else{
+                res.status(201).json(result)
+            }
+        })
+    }
+
+    search(res, value){
+        const sql = `SELECT * FROM product WHERE product_name like "%${value}%"`
+        connection.query(sql, value, (erro, result) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json( result)
+            }
+        })
+    }
 }
 
 module.exports = new Stock()
