90c9ac9c03214013e6fb9c04b72343b676c9df91
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2022-04-12 18:02:09 -0300

    docker-compose implementado

--- FrontEnd/App.jsx
+++ FrontEnd/App.jsx
@@ -3,7 +3,6 @@ import Stock from './screens/Stock';
 import AppLoading from 'expo-app-loading';
 import { useFonts, NunitoSans_400Regular, NunitoSans_600SemiBold, NunitoSans_700Bold, NunitoSans_900Black } from '@expo-google-fonts/nunito-sans';
 
-
 export default function App() {  
   let [fontsLoaded] = useFonts({
     NunitoSans_400Regular, NunitoSans_600SemiBold, NunitoSans_700Bold, NunitoSans_900Black 
